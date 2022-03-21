8fc57eff8d16037fd43ebac6c5b179337a01d44b
mcpca <aguiar@kth.se>
2022-03-21 15:49:29 +0100

    add reading hyperparams from command line

---
+++ utils.py
@@ -0,0 +1,86 @@
+import argparse
+from argparse import ArgumentParser
+
+
+def parse_args():
+    ap = ArgumentParser()
+
+    ap.add_argument('--control_delta',
+                    type=positive_float,
+                    help="Control sampling rate",
+                    default=0.5)
+
+    ap.add_argument('--time_horizon',
+                    type=positive_float,
+                    help="Time horizon",
+                    default=10.)
+
+    ap.add_argument('--n_trajectories',
+                    type=positive_int,
+                    help="Number of trajectories to sample",
+                    default=100)
+
+    ap.add_argument('--n_samples',
+                    type=positive_int,
+                    help="Number of state samples per trajectory",
+                    default=50)
+
+    ap.add_argument(
+        '--train_val_split',
+        type=percentage,
+        help="Percentage of the generated data that is used for training",
+        default=70)
+
+    ap.add_argument('--batch_size',
+                    type=positive_int,
+                    help="Batch size for training and validation",
+                    default=256)
+
+    ap.add_argument('--control_rnn_size',
+                    type=positive_int,
+                    help="Size of the RNN hidden state",
+                    default=6)
+
+    ap.add_argument('--lr',
+                    type=positive_float,
+                    help="Initial learning rate",
+                    default=1e-3)
+
+    ap.add_argument('--n_epochs',
+                    type=positive_int,
+                    help="Max number of epochs",
+                    default=10000)
+
+    ap.add_argument('--save_model',
+                    type=str,
+                    help="Path to write .pth model",
+                    default=None)
+
+    return ap.parse_args()
+
+
+def positive_int(value):
+    value = int(value)
+
+    if value <= 0:
+        raise argparse.ArgumentTypeError(f"{value} is not a positive integer")
+
+    return value
+
+
+def positive_float(value):
+    value = float(value)
+
+    if value <= 0:
+        raise argparse.ArgumentTypeError(f"{value} is not a positive float")
+
+    return value
+
+
+def percentage(value):
+    value = int(value)
+
+    if not (0 <= value <= 100):
+        raise argparse.ArgumentTypeError(f"{value} is not a valid percentage")
+
+    return value
