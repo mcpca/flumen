d3bc9eb817f8826ebb46d8c41d3ab3ddf4a3cdf7
mcpca <aguiar@kth.se>
2022-03-22 16:43:23 +0100

    dynamics: add missing Van der Pol class

--- dynamics.py
+++ dynamics.py
@@ -6,6 +6,7 @@ class Dynamics:
     def __call__(self, x, u):
         return self._dx(x, u)
 
+
 class LinearSys(Dynamics):
     def __init__(self, a, b):
         super().__init__(a.shape[0], b.shape[1])
@@ -15,3 +16,17 @@ class LinearSys(Dynamics):
 
     def _dx(self, x, u):
         return self.a @ x + self.b @ u
+
+
+class VanDerPol(Dynamics):
+    def __init__(self, damping):
+        super().__init__(2, 1)
+        self.damping = damping
+
+    def _dx(self, x, u):
+        p, v = x
+
+        dp = v
+        dv = -p + (self.damping**2) * (1 - p**2) * v
+
+        return (dp, dv)
