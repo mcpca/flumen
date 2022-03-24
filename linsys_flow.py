cc3a578724a2f9f1136e1973bc122cea44e1707d
mcpca <aguiar@kth.se>
2022-03-24 16:01:50 +0100

    move simulation and training to separate module

--- linsys_flow.py
+++ linsys_flow.py
@@ -1,18 +1,8 @@
-import torch
-
-torch.set_default_dtype(torch.float32)
-
-from torch import nn
-from torch.utils.data import DataLoader, random_split
 import numpy as np
-from trajectory import (TrajectoryGenerator, TrajectoryDataset,
-                        RandomWalkSequence)
-from flow_model import CausalFlowModel
-from train import EarlyStopping, train, validate
+from trajectory import RandomWalkSequence
 from utils import parse_args, print_gpu_info
 from dynamics import LinearSys
-
-import time
+from sim_and_train import sim_and_train
 
 
 def main():
@@ -21,80 +11,9 @@ def main():
     a_matrix = np.array([[-0.01, 1], [0, -1]])
     b_matrix = np.array([0, 1]).reshape((-1, 1))
     dynamics = LinearSys(a_matrix, b_matrix)
+    control_generator = RandomWalkSequence()
 
-    trajectory_generator = TrajectoryGenerator(
-        dynamics,
-        control_generator=RandomWalkSequence(),
-        control_delta=args.control_delta)
-
-    traj_data = TrajectoryDataset(trajectory_generator,
-                                  n_trajectories=args.n_trajectories,
-                                  n_samples=args.n_samples,
-                                  time_horizon=args.time_horizon)
-
-    norm_center, norm_weight = traj_data.whiten_targets()
-
-    train_len = int((float(args.train_val_split) / 100.) * len(traj_data))
-    train_data, val_data = random_split(traj_data,
-                                        lengths=(train_len,
-                                                 len(traj_data) - train_len))
-
-    train_dl = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
-    val_dl = DataLoader(val_data, batch_size=args.batch_size, shuffle=False)
-
-    model = CausalFlowModel(state_dim=dynamics.n,
-                            control_dim=dynamics.m,
-                            control_rnn_size=args.control_rnn_size,
-                            delta=args.control_delta,
-                            norm_center=norm_center,
-                            norm_weight=norm_weight,
-                            generator=trajectory_generator)
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    if torch.cuda.is_available() and torch.cuda.device_count() > 1:
-        model = nn.DataParallel(model)
-
-    model.to(device)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    sched = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, cooldown=2)
-    mse_loss = nn.MSELoss().to(device)
-
-    early_stop = EarlyStopping(es_patience=args.es_patience,
-                               es_delta=args.es_delta)
-
-    print('Epoch :: Loss (Train) :: Loss (Val) :: Best (Val)')
-    print('=================================================')
-
-    start = time.time()
-
-    for epoch in range(args.n_epochs):
-        loss = 0.
-
-        model.train()
-        for example in train_dl:
-            loss += train(example, mse_loss, model, optimizer, epoch, device)
-
-        loss /= len(train_dl)
-
-        model.eval()
-        val_loss = validate(val_dl, mse_loss, model, device)
-        sched.step(val_loss)
-        early_stop.step(val_loss)
-
-        print(
-            f"{epoch + 1:>5d} :: {loss:>7e} :: {val_loss:>7e} :: {early_stop.best_val_loss:>7e}"
-        )
-
-        if early_stop.best_model and args.save_model:
-            torch.save(model, f'outputs/{args.save_model}')
-
-        if early_stop.early_stop:
-            break
-
-    train_time = time.time() - start
-    print(f"Training took {train_time:.2f} seconds.")
+    sim_and_train(args, dynamics, control_generator)
 
 
 if __name__ == '__main__':
---
+++ sim_and_train.py
@@ -0,0 +1,125 @@
+import torch
+
+torch.set_default_dtype(torch.float32)
+
+from torch.utils.data import DataLoader, random_split
+from trajectory import (TrajectoryGenerator, TrajectoryDataset,
+                        SequenceGenerator)
+from flow_model import CausalFlowModel
+from train import EarlyStopping, train, validate
+from dynamics import Dynamics
+
+import time
+
+
+def simulate(dynamics: Dynamics, control_generator: SequenceGenerator,
+             control_delta, n_trajectories, n_samples, time_horizon):
+    trajectory_generator = TrajectoryGenerator(
+        dynamics,
+        control_delta=control_delta,
+        control_generator=control_generator)
+
+    traj_data = TrajectoryDataset(trajectory_generator,
+                                  n_trajectories=n_trajectories,
+                                  n_samples=n_samples,
+                                  time_horizon=time_horizon)
+
+    return traj_data, trajectory_generator
+
+
+def preprocess(traj_data, batch_size, split):
+    norm_center, norm_weight = traj_data.whiten_targets()
+
+    train_len = int((float(split) / 100.) * len(traj_data))
+    train_data, val_data = random_split(traj_data,
+                                        lengths=(train_len,
+                                                 len(traj_data) - train_len))
+
+    train_dl = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    val_dl = DataLoader(val_data, batch_size=batch_size, shuffle=False)
+
+    return train_dl, val_dl, norm_center, norm_weight
+
+
+def training_loop(model, loss_fn, optimizer, sched, early_stop, train_dl,
+                  val_dl, device, max_epochs, model_save_path):
+    print('Epoch :: Loss (Train) :: Loss (Val) :: Best (Val)')
+    print('=================================================')
+
+    start = time.time()
+
+    for epoch in range(max_epochs):
+        loss = 0.
+
+        model.train()
+        for example in train_dl:
+            loss += train(example, loss_fn, model, optimizer, epoch, device)
+
+        loss /= len(train_dl)
+
+        model.eval()
+        val_loss = validate(val_dl, loss_fn, model, device)
+        sched.step(val_loss)
+        early_stop.step(val_loss)
+
+        print(
+            f"{epoch + 1:>5d} :: {loss:>7e} :: {val_loss:>7e} :: {early_stop.best_val_loss:>7e}"
+        )
+
+        if early_stop.best_model and model_save_path:
+            torch.save(model, f'outputs/{model_save_path}')
+
+        if early_stop.early_stop:
+            break
+
+    train_time = time.time() - start
+
+    return loss, val_loss, train_time
+
+
+def sim_and_train(args, dynamics: Dynamics,
+                  control_generator: SequenceGenerator):
+    traj_data, traj_generator = simulate(dynamics,
+                                         control_generator,
+                                         control_delta=args.control_delta,
+                                         n_trajectories=args.n_trajectories,
+                                         n_samples=args.n_samples,
+                                         time_horizon=args.time_horizon)
+
+    train_dl, val_dl, norm_center, norm_weight = preprocess(
+        traj_data, batch_size=args.batch_size, split=args.train_val_split)
+
+    model = CausalFlowModel(state_dim=dynamics.n,
+                            control_dim=dynamics.m,
+                            control_rnn_size=args.control_rnn_size,
+                            delta=args.control_delta,
+                            norm_center=norm_center,
+                            norm_weight=norm_weight,
+                            generator=traj_generator)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    if torch.cuda.is_available() and torch.cuda.device_count() > 1:
+        model = torch.nn.DataParallel(model)
+
+    model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    sched = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, cooldown=2)
+    mse_loss = torch.nn.MSELoss().to(device)
+
+    early_stop = EarlyStopping(es_patience=args.es_patience,
+                               es_delta=args.es_delta)
+
+    loss, val_loss, train_time = training_loop(model,
+                                               mse_loss,
+                                               optimizer,
+                                               sched,
+                                               early_stop,
+                                               train_dl,
+                                               val_dl,
+                                               device,
+                                               max_epochs=args.n_epochs,
+                                               model_save_path=args.save_model)
+
+    print(f"Training took {train_time:.2f} seconds.")
