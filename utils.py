74dd09c01be9986a1d3499b5a026858c8ca1413d
mcpca <mca@netc.eu>
2022-07-14 17:28:54 +0200

    wip

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import torch
 
-import argparse
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentTypeError
+
+import sys, uuid, subprocess, time, datetime, os
 
 
 def parse_args():
@@ -75,7 +76,7 @@ def parse_args():
 
     ap.add_argument('--save_model',
                     type=str,
-                    help="Path to write .pth model",
+                    help="Subdirectory where the model will be saved",
                     default=None)
 
     ap.add_argument('--save_data',
@@ -95,7 +96,7 @@ def positive_int(value):
     value = int(value)
 
     if value <= 0:
-        raise argparse.ArgumentTypeError(f"{value} is not a positive integer")
+        raise ArgumentTypeError(f"{value} is not a positive integer")
 
     return value
 
@@ -104,7 +105,7 @@ def positive_float(value):
     value = float(value)
 
     if value <= 0:
-        raise argparse.ArgumentTypeError(f"{value} is not a positive float")
+        raise ArgumentTypeError(f"{value} is not a positive float")
 
     return value
 
@@ -113,7 +114,7 @@ def nonnegative_float(value):
     value = float(value)
 
     if value < 0:
-        raise argparse.ArgumentTypeError(f"{value} is not a nonnegative float")
+        raise ArgumentTypeError(f"{value} is not a nonnegative float")
 
     return value
 
@@ -122,9 +123,10 @@ def percentage(value):
     value = int(value)
 
     if not (0 <= value <= 100):
-        raise argparse.ArgumentTypeError(f"{value} is not a valid percentage")
+        raise ArgumentTypeError(f"{value} is not a valid percentage")
 
     return value
+
 
 def print_gpu_info():
     if torch.cuda.is_available():
@@ -139,3 +141,60 @@ def print_gpu_info():
                 msg += " [Current]"
 
             print(msg)
+
+
+def save_path(args, id):
+    if not args.save_model:
+        return None
+
+    file_name = args.save_model + str(id.hex)
+    path = os.path.join(os.path.dirname(__file__), 'outputs',
+                        file_name + '.pth')
+
+    return path, file_name
+
+
+class TrainedModel:
+
+    def __init__(self, args):
+        self.__id = uuid.uuid4()
+
+        self.cmd = ' '.join(sys.argv)
+        self.args = args
+        self.timestamp = None
+
+        try:
+            self.git_head = subprocess.check_output(
+                ['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+        except:
+            self.git_head = None
+
+        if args.save_model:
+            self.save_path, self.file_name = save_path(args)
+        else:
+            self.save_path = None
+            self.file_name = None
+
+        if args.load_data:
+            self.data_path = os.path.abspath(args.load_data)
+        else:
+            self.data_path = None
+
+        self.flow_model = None
+
+    def set_model(self, model):
+        self.flow_model = model
+        self.timestamp = time.time()
+
+    def save(self):
+        torch.save(self, self.save_path)
+
+    def __str__(self):
+        out_str = f'''\
+            --- Trained model {self.file_name}
+                Timestamp: {datetime.datetime.fromtimestamp(self.timestamp).strftime('%Y/%m/%d %H:%M:%s')}
+                Git hash: {self.git_head if self.git_head else 'N/A'}
+                Command line: {self.cmd}
+                Data: {self.data_path if self.data_path else 'N/A'}
+        '''
+        return out_str
