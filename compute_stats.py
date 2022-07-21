d28c0750f1f4b70d96c0c0a81fd1dc8f4fd32178
mcpca <mca@netc.eu>
2022-07-21 17:31:30 +0200

    Add script to compute statistics
    For Monte Carlo runs.

---
+++ compute_stats.py
@@ -0,0 +1,133 @@
+from argparse import ArgumentParser
+import os, csv, re
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+import matplotlib.pyplot as plt
+
+from meta import Meta
+
+
+def parse_args():
+    ap = ArgumentParser()
+
+    ap.add_argument('dir',
+                    type=str,
+                    help="Directory containing the models to be tested")
+    ap.add_argument('test_data', type=str, help="Test dataset.")
+
+    return ap.parse_args()
+
+    return
+
+
+def main():
+    args = parse_args()
+
+    # test_data: Dataset = torch.load(args.test_data)
+    # test_dl = DataLoader(test_data, batch_size=1024, shuffle=True)
+
+    stats = (TrainTime(), TrainError(), ValError())
+    stat_vals = {}
+    for stat in stats:
+        stat_vals[str(stat)] = []
+
+
+    file_prefix = os.path.split(args.dir)[-1]
+
+    file_matcher = re.compile(file_prefix +
+                              '_[0-9]{8}_[0-9]{6}_[0-9a-f]{32}.pth')
+
+    for fname in os.listdir(args.dir):
+        if not file_matcher.match(fname):
+            continue
+
+        load_path = os.path.join(args.dir, fname)
+        meta: Meta = torch.load(load_path, map_location=torch.device('cpu'))
+        meta.args.save_model = file_prefix  # DEBUG -- remove me later
+        meta.set_root(os.path.dirname(__file__))
+
+        for stat in stats:
+            stat_vals[str(stat)].append(stat(meta))
+
+    labels, data = stat_vals.keys(), stat_vals.values()
+    plt.boxplot(data)
+    plt.xticks(range(1, len(labels) + 1), labels)
+    plt.show()
+
+
+class TestError:
+
+    def __init__(self, test_dl: DataLoader):
+        self.loss = torch.nn.MSELoss
+        self.data = test_dl
+
+    def __call__(self, meta: Meta):
+        model = meta.load_model()
+
+        def eval_fn(t, x, u):
+            return meta.predict(model, t, x, u)
+
+        rv = 0.
+
+        with torch.no_grad():
+            for (x0, t, y, u, lengths) in self.data:
+                sort_idxs = torch.argsort(lengths, descending=True)
+
+                x0 = x0[sort_idxs]
+                t = t[sort_idxs]
+                y = y[sort_idxs]
+                u = u[sort_idxs]
+                lengths = lengths[sort_idxs]
+
+                u = torch.nn.utils.rnn.pack_padded_sequence(
+                    u, lengths, batch_first=True, enforce_sorted=True)
+
+                y_pred = eval_fn(t, x0, u)
+                rv += self.loss(y, y_pred)
+
+        return rv / len(self.data)
+
+    def __str__(self):
+        return 'test_mse'
+
+
+class ValError:
+
+    def __init__(self):
+        pass
+
+    def __call__(self, meta: Meta):
+        return meta.val_loss_best
+
+    def __str__(self):
+        return 'val_mse'
+
+
+class TrainError:
+
+    def __init__(self):
+        pass
+
+    def __call__(self, meta: Meta):
+        return meta.train_loss_best
+
+    def __str__(self):
+        return 'train_mse'
+
+
+class TrainTime:
+
+    def __init__(self):
+        pass
+
+    def __call__(self, meta: Meta):
+        return meta.train_time
+
+    def __str__(self):
+        return 'train_time'
+
+
+if __name__ == "__main__":
+    main()
