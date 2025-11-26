b0d2e50107e6fbed8ac3d6b51912f65f652d1f00
mcpca <mcpcaguiar@gmail.com>
2025-11-26 16:42:39 +0100

    Update according to changes in semble

--- experiments/interactive_test.py
+++ experiments/interactive_test.py
@@ -4,7 +4,8 @@ import numpy as np
 
 from flumen import CausalFlowModel
 from flumen.utils import pack_model_inputs
-from generate_data import make_trajectory_sampler
+from semble import make_trajectory_sampler, TSamplerSpec
+from semble.dynamics import ContinuousStateDynamics
 
 from argparse import ArgumentParser
 
@@ -13,21 +14,25 @@ from pathlib import Path
 import sys
 from pprint import pprint
 from time import time
+from typing import cast
 
 
 def parse_args():
     ap = ArgumentParser()
     ap.add_argument(
-        'path',
+        "path",
         type=str,
         help="Path to .pth file "
-        "(or, if run with --wandb, path to a Weights & Biases artifact)")
-    ap.add_argument('--print_info',
-                    action='store_true',
-                    help="Print training metadata and quit")
-    ap.add_argument('--continuous_state', action='store_true')
-    ap.add_argument('--wandb', action='store_true')
-    ap.add_argument('--time_horizon', type=float, default=None)
+        "(or, if run with --wandb, path to a Weights & Biases artifact)",
+    )
+    ap.add_argument(
+        "--print_info",
+        action="store_true",
+        help="Print training metadata and quit",
+    )
+    ap.add_argument("--continuous_state", action="store_true")
+    ap.add_argument("--wandb", action="store_true")
+    ap.add_argument("--time_horizon", type=float, default=None)
 
     return ap.parse_args()
 
@@ -37,18 +42,20 @@ def main():
 
     if args.wandb:
         import wandb
+
         api = wandb.Api()
         model_artifact = api.artifact(args.path)
         model_path = Path(model_artifact.download())
 
         model_run = model_artifact.logged_by()
-        print(model_run.summary)
+        if model_run:
+            pprint(model_run.summary)
     else:
         model_path = Path(args.path)
 
-    with open(model_path / "state_dict.pth", 'rb') as f:
-        state_dict = torch.load(f, weights_only=False, map_location='cpu')
-    with open(model_path / "metadata.yaml", 'r') as f:
+    with open(model_path / "state_dict.pth", "rb") as f:
+        state_dict = torch.load(f, weights_only=False, map_location="cpu")
+    with open(model_path / "metadata.yaml", "r") as f:
         metadata: dict = yaml.load(f, Loader=yaml.FullLoader)
 
     pprint(metadata)
@@ -60,42 +67,47 @@ def main():
     model.load_state_dict(state_dict)
     model.eval()
 
-    sampler = make_trajectory_sampler(metadata["data_settings"])
+    sampler_spec: TSamplerSpec = metadata["data_settings"]
+    sampler = make_trajectory_sampler(sampler_spec)
     sampler.reset_rngs()
     delta = sampler._delta
 
     if args.continuous_state:
-        xx = np.linspace(0., 1., model.output_dim)
+        dynamics = cast(ContinuousStateDynamics, sampler._dyn)
+        xx = dynamics.get_space_axis()
         n_plots = 2
     else:
         n_plots = model.output_dim
+        xx = None
 
     fig, ax = plt.subplots(n_plots + 1, 1, sharex=True)
-    fig.canvas.mpl_connect('close_event', on_close_window)
+    fig.canvas.mpl_connect("close_event", on_close_window)
 
-    time_horizon = args.time_horizon if args.time_horizon else metadata[
-        "data_args"]["time_horizon"]
+    time_horizon = (
+        args.time_horizon
+        if args.time_horizon
+        else metadata["data_args"]["time_horizon"]
+    )
 
     while True:
         time_integrate = time()
-        x0, t, y, u = sampler.get_example(time_horizon=time_horizon,
-                                          n_samples=int(1 +
-                                                        100 * time_horizon))
+        x0, t, y, u = sampler.get_example(
+            time_horizon=time_horizon, n_samples=int(1 + 3 * time_horizon)
+        )
 
         time_integrate = time() - time_integrate
 
-        time_predict = time()
-
-        x0_feed, u_feed, skips, tau = pack_model_inputs(x0, t, u, delta)
-
         with torch.no_grad():
-            y_pred = model.forward_trajectory(x0_feed, u_feed, skips,
-                                              tau).numpy()
+            time_predict = time()
 
-        time_predict = time() - time_predict
+            x0_feed, u_feed, skips, tau = pack_model_inputs(x0, t, u, delta)
+            y_pred = model.forward_trajectory(x0_feed, u_feed, skips, tau)
+
+            time_predict = time() - time_predict
 
         print(f"Timings: {time_integrate}, {time_predict}")
 
+        y_pred = y_pred.numpy()
         y = y[:, tuple(bool(v) for v in sampler._dyn.mask)]
 
         sq_error = np.square(y - y_pred)
@@ -105,12 +117,12 @@ def main():
             ax[0].pcolormesh(t.squeeze(), xx, y.T)
             ax[1].pcolormesh(t.squeeze(), xx, y_pred.T)
         else:
-            for k, ax_ in enumerate(ax[:model.output_dim]):
-                ax_.plot(t, y_pred[:, k], c='orange', label='Model output')
-                ax_.plot(t, y[:, k], 'b--', label='True state')
-                ax_.set_ylabel(f"$x_{k+1}$")
+            for k, ax_ in enumerate(ax[: model.output_dim]):
+                ax_.plot(t, y_pred[:, k], c="orange", label="Model output")
+                ax_.plot(t, y[:, k], "b--", label="True state")
+                ax_.set_ylabel(f"$x_{k + 1}$")
 
-        ax[-1].step(np.arange(0., time_horizon, delta), u[:-1], where='post')
+        ax[-1].step(np.arange(0.0, time_horizon, delta), u[:-1], where="post")
         ax[-1].set_ylabel("$u$")
         ax[-1].set_xlabel("$t$")
 
@@ -129,9 +141,9 @@ def main():
             ax_.clear()
 
 
-def on_close_window(ev):
+def on_close_window(_):
     sys.exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- experiments/semble_generate.py
+++ experiments/semble_generate.py
@@ -4,24 +4,26 @@ torch.set_default_dtype(torch.float32)
 
 import pickle, yaml
 from pathlib import Path
+from argparse import ArgumentParser, ArgumentTypeError
 
 from scipy.signal import find_peaks
 
-from generate_data import parse_args, generate, make_trajectory_sampler
+from semble import TrajectorySampler, TSamplerSpec, make_trajectory_sampler
+from flumen import RawTrajectoryDataset
 
 
 def main():
     args = parse_args()
 
-    with open(args.settings, 'r') as f:
-        settings: dict = yaml.load(f, Loader=yaml.FullLoader)
+    with open(args.settings, "r") as f:
+        settings: TSamplerSpec = yaml.load(f, Loader=yaml.FullLoader)
 
     sampler = make_trajectory_sampler(settings)
     postprocess = get_postprocess(settings["dynamics"]["name"])
 
-    train_data, val_data, test_data = generate(args,
-                                               sampler,
-                                               postprocess=postprocess)
+    train_data, val_data, test_data = generate(
+        args, sampler, postprocess=postprocess
+    )
 
     data = {
         "train": train_data,
@@ -35,7 +37,7 @@ def main():
     output_dir.mkdir(exist_ok=True)
 
     # Write to disk
-    with open(output_dir.joinpath(args.output_name + ".pkl"), 'wb') as f:
+    with open(output_dir.joinpath(args.output_name + ".pkl"), "wb") as f:
         pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 
@@ -53,7 +55,7 @@ def get_postprocess(dynamics: str):
 
 
 def rejection_sampling_single_neuron(data):
-    for (k, y) in enumerate(data.state):
+    for k, y in enumerate(data.state):
         p = y[:, 0].flatten()
         p_min = p.min()
         p = 1e-4 + ((p - p_min) / (p.max() - p_min))
@@ -61,8 +63,8 @@ def rejection_sampling_single_neuron(data):
         likelihood_ratio = p / torch.mean(p)
         lr_bound = torch.max(likelihood_ratio)
 
-        u = torch.rand((len(likelihood_ratio), ))
-        keep_idxs = (u <= (likelihood_ratio / lr_bound))
+        u = torch.rand((len(likelihood_ratio),))
+        keep_idxs = u <= (likelihood_ratio / lr_bound)
         keep_idxs[0] = True
 
         peaks, _ = find_peaks(y[:, 0])
@@ -74,22 +76,22 @@ def rejection_sampling_single_neuron(data):
 
 
 def rejection_sampling_two_neuron(data):
-    for (k, y) in enumerate(data.state):
+    for k, y in enumerate(data.state):
         p_1 = y[:, 0].flatten()
         p_min = p_1.min()
-        p_1 = ((p_1 - p_min) / (p_1.max() - p_min))
+        p_1 = (p_1 - p_min) / (p_1.max() - p_min)
 
         p_2 = y[:, 5].flatten()
         p_min = p_2.min()
-        p_2 = ((p_2 - p_min) / (p_2.max() - p_min))
+        p_2 = (p_2 - p_min) / (p_2.max() - p_min)
 
         p = torch.maximum(p_1, p_2)
 
         likelihood_ratio = p / torch.mean(p)
         lr_bound = torch.max(likelihood_ratio)
 
-        u = torch.rand((len(likelihood_ratio), ))
-        keep_idxs = (u <= (likelihood_ratio / lr_bound))
+        u = torch.rand((len(likelihood_ratio),))
+        keep_idxs = u <= (likelihood_ratio / lr_bound)
         keep_idxs[0] = True
 
         for y_spiking in (y[:, 0], y[:, 5]):
@@ -101,5 +103,126 @@ def rejection_sampling_two_neuron(data):
         data.time[k] = data.time[k][keep_idxs, :]
 
 
-if __name__ == '__main__':
+def percentage(value):
+    value = int(value)
+
+    if not (0 <= value <= 100):
+        raise ArgumentTypeError(f"{value} is not a valid percentage")
+
+    return value
+
+
+def parse_args():
+    ap = ArgumentParser()
+
+    ap.add_argument(
+        "settings",
+        type=str,
+        help="Path to a YAML file containing the parameters"
+        " defining the trajectory sampler.",
+    )
+
+    ap.add_argument(
+        "output_name", type=str, help="File name for writing the data to disk."
+    )
+
+    ap.add_argument(
+        "--time_horizon", type=float, help="Time horizon", default=10.0
+    )
+
+    ap.add_argument(
+        "--n_trajectories",
+        type=int,
+        help="Number of trajectories to sample",
+        default=100,
+    )
+
+    ap.add_argument(
+        "--n_samples",
+        type=int,
+        help="Number of state samples per trajectory",
+        default=50,
+    )
+
+    ap.add_argument(
+        "--noise_std",
+        type=float,
+        help="Standard deviation of measurement noise",
+        default=0.0,
+    )
+
+    ap.add_argument(
+        "--noise_seed", type=int, help="Measurement noise seed", default=None
+    )
+
+    ap.add_argument(
+        "--data_split",
+        nargs=2,
+        type=percentage,
+        help="Percentage of data used for validation and test sets",
+        default=[20, 20],
+    )
+
+    return ap.parse_args()
+
+
+def generate(args, trajectory_sampler: TrajectorySampler, postprocess=[]):
+    if args.data_split[0] + args.data_split[1] >= 100:
+        raise Exception("Invalid data split.")
+
+    n_val = int(args.n_trajectories * (args.data_split[0] / 100.0))
+    n_test = int(args.n_trajectories * (args.data_split[1] / 100.0))
+    n_train = args.n_trajectories - n_val - n_test
+
+    def get_example():
+        x0, t, y, u = trajectory_sampler.get_example(
+            args.time_horizon, args.n_samples
+        )
+        return {
+            "init_state": x0,
+            "time": t,
+            "state": y,
+            "control": u,
+        }
+
+    train_data = [get_example() for _ in range(n_train)]
+    trajectory_sampler.reset_rngs()
+
+    val_data = [get_example() for _ in range(n_val)]
+    trajectory_sampler.reset_rngs()
+
+    test_data = [get_example() for _ in range(n_test)]
+
+    train_data = RawTrajectoryDataset(
+        train_data,
+        *trajectory_sampler.dims(),
+        delta=trajectory_sampler._delta,
+        output_mask=trajectory_sampler._dyn.mask,
+        noise_std=args.noise_std,
+    )
+
+    val_data = RawTrajectoryDataset(
+        val_data,
+        *trajectory_sampler.dims(),
+        delta=trajectory_sampler._delta,
+        output_mask=trajectory_sampler._dyn.mask,
+        noise_std=args.noise_std,
+    )
+
+    test_data = RawTrajectoryDataset(
+        test_data,
+        *trajectory_sampler.dims(),
+        delta=trajectory_sampler._delta,
+        output_mask=trajectory_sampler._dyn.mask,
+        noise_std=args.noise_std,
+    )
+
+    for d in (train_data, val_data, test_data):
+        for p in postprocess:
+            p(d)
+
+    return train_data, val_data, test_data
+
+
+if __name__ == "__main__":
     main()
