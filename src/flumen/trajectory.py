b441f8d236b61219fa17f80f63843d9b8ba8e431
mcpca <mcpcaguiar@gmail.com>
2025-12-03 20:12:36 +0100

    trajectory: Remove obsolete method.

--- src/flumen/trajectory.py
+++ src/flumen/trajectory.py
@@ -4,16 +4,17 @@ from torch.utils.data import Dataset
 
 
 class RawTrajectoryDataset(Dataset):
-
-    def __init__(self,
-                 data,
-                 state_dim,
-                 control_dim,
-                 output_dim,
-                 delta,
-                 output_mask,
-                 noise_std=0.,
-                 **kwargs):
+    def __init__(
+        self,
+        data,
+        state_dim,
+        control_dim,
+        output_dim,
+        delta,
+        output_mask,
+        noise_std=0.0,
+        **kwargs,
+    ):
         self.__dict__.update(kwargs)
 
         n_traj = len(data)
@@ -23,10 +24,12 @@ class RawTrajectoryDataset(Dataset):
         self.delta = delta
         self.mask = output_mask
 
-        self.init_state = torch.empty(
-            (n_traj, self.state_dim)).type(torch.get_default_dtype())
-        self.init_state_noise = torch.empty(
-            (n_traj, self.state_dim)).type(torch.get_default_dtype())
+        self.init_state = torch.empty((n_traj, self.state_dim)).type(
+            torch.get_default_dtype()
+        )
+        self.init_state_noise = torch.empty((n_traj, self.state_dim)).type(
+            torch.get_default_dtype()
+        )
 
         self.time = []
         self.state = []
@@ -34,64 +37,52 @@ class RawTrajectoryDataset(Dataset):
         self.control_seq = []
 
         for k, sample in enumerate(data):
-            self.init_state[k] = torch.from_numpy(sample["init_state"].reshape(
-                (1, self.state_dim)))
-            self.init_state_noise[k] = 0.
+            self.init_state[k] = torch.from_numpy(
+                sample["init_state"].reshape((1, self.state_dim))
+            )
+            self.init_state_noise[k] = 0.0
             self.time.append(
-                torch.from_numpy(sample["time"]).type(
-                    torch.get_default_dtype()).reshape((-1, 1)))
+                torch.from_numpy(sample["time"])
+                .type(torch.get_default_dtype())
+                .reshape((-1, 1))
+            )
 
             self.state.append(
-                torch.from_numpy(sample["state"]).type(
-                    torch.get_default_dtype()).reshape((-1, self.state_dim)))
+                torch.from_numpy(sample["state"])
+                .type(torch.get_default_dtype())
+                .reshape((-1, self.state_dim))
+            )
 
             self.state_noise.append(
-                torch.normal(mean=0.,
-                             std=noise_std,
-                             size=self.state[-1].size()))
+                torch.normal(
+                    mean=0.0, std=noise_std, size=self.state[-1].size()
+                )
+            )
 
             self.control_seq.append(
-                torch.from_numpy(sample["control"]).type(
-                    torch.get_default_dtype()).reshape((-1, self.control_dim)))
-
-    @classmethod
-    def generate(cls, generator, time_horizon, n_trajectories, n_samples,
-                 noise_std):
-
-        def get_example():
-            x0, t, y, u = generator.get_example(time_horizon, n_samples)
-            return {
-                "init_state": x0,
-                "time": t,
-                "state": y,
-                "control": u,
-            }
-
-        data = [get_example() for _ in range(n_trajectories)]
-
-        return cls(data,
-                   *generator.dims(),
-                   delta=generator._delta,
-                   output_mask=generator._dyn.mask,
-                   generator=generator,
-                   noise_std=noise_std)
+                torch.from_numpy(sample["control"])
+                .type(torch.get_default_dtype())
+                .reshape((-1, self.control_dim))
+            )
 
     def __len__(self):
         return len(self.init_state)
 
     def __getitem__(self, index):
-        return (self.init_state[index], self.init_state_noise[index],
-                self.time[index], self.state[index], self.state_noise[index],
-                self.control_seq[index])
+        return (
+            self.init_state[index],
+            self.init_state_noise[index],
+            self.time[index],
+            self.state[index],
+            self.state_noise[index],
+            self.control_seq[index],
+        )
 
 
 class TrajectoryDataset(Dataset):
-
-    def __init__(self,
-                 raw_data: RawTrajectoryDataset,
-                 max_seq_len=-1,
-                 n_samples=1):
-
+    def __init__(
+        self, raw_data: RawTrajectoryDataset, max_seq_len=-1, n_samples=1
+    ):
         self.state_dim = raw_data.state_dim
         self.control_dim = raw_data.control_dim
         self.output_dim = raw_data.output_dim
@@ -108,14 +99,15 @@ class TrajectoryDataset(Dataset):
 
         k_tr = 0
 
-        for (x0, x0_n, t, y, y_n, u) in raw_data:
+        for x0, x0_n, t, y, y_n, u in raw_data:
             y += y_n
             x0 += x0_n
 
             if max_seq_len == -1:
                 for k_s, y_s in enumerate(y):
                     rnn_input, rnn_input_len = self.process_example(
-                        0, k_s, t, u, self.delta)
+                        0, k_s, t, u, self.delta
+                    )
 
                     s = y_s.view(1, -1)[:, mask].reshape(-1)
 
@@ -127,20 +119,23 @@ class TrajectoryDataset(Dataset):
             else:
                 for k_s, y_s in enumerate(y):
                     # find index of last relevant state sample
-                    times = (t - t[k_s] - max_seq_len * self.delta)
-                    times[times > 0] = 0.
+                    times = t - t[k_s] - max_seq_len * self.delta
+                    times[times > 0] = 0.0
                     k_l = times.argmax().item()
 
                     if k_l == k_s:
-                        end_idxs = (0, )
+                        end_idxs = (0,)
                     else:
-                        end_idxs = rng.choice(k_l - k_s,
-                                              size=min(n_samples, k_l - k_s),
-                                              replace=False)
+                        end_idxs = rng.choice(
+                            k_l - k_s,
+                            size=min(n_samples, k_l - k_s),
+                            replace=False,
+                        )
 
                     for k_e in end_idxs:
                         rnn_input, rnn_input_len = self.process_example(
-                            k_s, k_s + k_e, t, u, self.delta)
+                            k_s, k_s + k_e, t, u, self.delta
+                        )
 
                         init_state.append(y_s)
                         state.append(y[k_s + k_e, mask])
@@ -148,36 +143,38 @@ class TrajectoryDataset(Dataset):
                         rnn_input_data.append(rnn_input)
 
         self.init_state = torch.stack(init_state).type(
-            torch.get_default_dtype())
+            torch.get_default_dtype()
+        )
         self.state = torch.stack(state).type(torch.get_default_dtype())
         self.rnn_input = torch.stack(rnn_input_data).type(
-            torch.get_default_dtype())
+            torch.get_default_dtype()
+        )
         self.seq_lens = torch.tensor(seq_len_data, dtype=torch.long)
 
         self.len = len(init_state)
 
     @staticmethod
     def process_example(start_idx, end_idx, t, u, delta):
-        init_time = 0.
+        init_time = 0.0
 
         u_start_idx = int(np.floor((t[start_idx] - init_time) / delta))
         u_end_idx = int(np.floor((t[end_idx] - init_time) / delta))
         u_sz = 1 + u_end_idx - u_start_idx
 
         u_seq = torch.zeros_like(u)
-        u_seq[0:u_sz] = u[u_start_idx:(u_end_idx + 1)]
+        u_seq[0:u_sz] = u[u_start_idx : (u_end_idx + 1)]
 
         deltas = torch.ones((u_seq.shape[0], 1))
         t_u_end = init_time + delta * u_end_idx
         t_u_start = init_time + delta * u_start_idx
 
         if u_sz > 1:
-            deltas[0] = (1. - (t[start_idx] - t_u_start) / delta).item()
+            deltas[0] = (1.0 - (t[start_idx] - t_u_start) / delta).item()
             deltas[u_sz - 1] = ((t[end_idx] - t_u_end) / delta).item()
         else:
             deltas[0] = ((t[end_idx] - t[start_idx]) / delta).item()
 
-        deltas[u_sz:] = 0.
+        deltas[u_sz:] = 0.0
 
         rnn_input = torch.hstack((u_seq, deltas))
 
@@ -187,5 +184,9 @@ class TrajectoryDataset(Dataset):
         return self.len
 
     def __getitem__(self, index):
-        return (self.init_state[index], self.state[index],
-                self.rnn_input[index], self.seq_lens[index])
+        return (
+            self.init_state[index],
+            self.state[index],
+            self.rnn_input[index],
+            self.seq_lens[index],
+        )
