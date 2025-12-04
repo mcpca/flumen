e7f7a393881e83258870efcf2845fa547c31631f
mcpca <mcpcaguiar@gmail.com>
2025-12-04 11:31:58 +0100

    train_wandb: cleanup + add timestamp + allow selecting outdir

--- experiments/train_wandb.py
+++ experiments/train_wandb.py
@@ -10,26 +10,27 @@ from flumen import CausalFlowModel, print_gpu_info, TrajectoryDataset
 from flumen.train import EarlyStopping, train_step, validate
 
 from argparse import ArgumentParser
-import time
+import datetime, time
 import re
+from sys import stderr
 
 import wandb
 
 hyperparams = {
-    'control_rnn_size': 64,
-    'control_rnn_depth': 1,
-    'encoder_size': 1,
-    'encoder_depth': 2,
-    'decoder_size': 1,
-    'decoder_depth': 2,
-    'batch_size': 128,
-    'lr': 7e-4,
-    'n_epochs': 2000,
-    'es_patience': 20,
-    'es_delta': 5e-5,
-    'sched_patience': 10,
-    'sched_factor': 2,
-    'loss': "mse",
+    "control_rnn_size": 64,
+    "control_rnn_depth": 1,
+    "encoder_size": 1,
+    "encoder_depth": 2,
+    "decoder_size": 1,
+    "decoder_depth": 2,
+    "batch_size": 128,
+    "lr": 7e-4,
+    "n_epochs": 2000,
+    "es_patience": 20,
+    "es_delta": 5e-5,
+    "sched_patience": 10,
+    "sched_factor": 2,
+    "loss": "mse",
 }
 
 
@@ -42,35 +43,54 @@ def get_loss(which):
         raise ValueError(f"Unknown loss {which}.")
 
 
+def print_header():
+    header_msg = (
+        f"{'Epoch':>5} :: {'Loss (Train)':>16} :: "
+        f"{'Loss (Val)':>16} :: {'Loss (Test)':>16} :: {'Best (Val)':>16}"
+    )
+
+    print(header_msg)
+    print("=" * len(header_msg))
+
+
+def print_losses(
+    epoch: int, train: float, val: float, test: float, best_val_yet: float
+):
+    print(
+        f"{epoch + 1:>5d} :: {train:>16e} :: {val:>16e} :: "
+        f"{test:>16e} :: {best_val_yet:>16e}"
+    )
+
+
+def get_timestamp() -> str:
+    now = datetime.datetime.now(tz=datetime.timezone.utc)
+    ts = now.strftime("%y%m%d_%H%M")
+
+    return ts
+
+
 def main():
     ap = ArgumentParser()
 
-    ap.add_argument('load_path', type=str, help="Path to trajectory dataset")
+    ap.add_argument("load_path", type=str, help="Path to trajectory dataset")
 
-    ap.add_argument('name', type=str, nargs='+', help="Name of the experiment.")
+    ap.add_argument("name", type=str, nargs="+", help="Name of the experiment.")
 
-    ap.add_argument('--reset_noise',
-                    action='store_true',
-                    help="Regenerate the measurement noise.")
+    ap.add_argument("--model_log_rate", type=int, default=15)
 
-    ap.add_argument('--noise_std',
-                    type=float,
-                    default=None,
-                    help="If reset_noise is set, set standard deviation ' \
-                            'of the measurement noise to this value.")
-
-    ap.add_argument('--model_log_rate', type=int, default=15)
+    ap.add_argument("--outdir", type=str, default="./outputs")
 
     sys_args = ap.parse_args()
     data_path = Path(sys_args.load_path)
 
     first_name = sys_args.name[0]
-    full_name = '_'.join(sys_args.name)
-    full_name = re.sub("[^a-zA-Z0-9_-]", '_', full_name)
+    timestamp = get_timestamp()
+    full_name = "_".join([timestamp] + sys_args.name)
+    full_name = re.sub("[^a-zA-Z0-9_-]", "_", full_name)
 
-    run = wandb.init(project='flumen', config=hyperparams, name=full_name)
+    run = wandb.init(project="flumen", config=hyperparams, name=full_name)
 
-    with data_path.open('rb') as f:
+    with data_path.open("rb") as f:
         data = pickle.load(f)
 
     train_data = TrajectoryDataset(data["train"])
@@ -78,79 +98,77 @@ def main():
     test_data = TrajectoryDataset(data["test"])
 
     model_args = {
-        'state_dim': train_data.state_dim,
-        'control_dim': train_data.control_dim,
-        'output_dim': train_data.output_dim,
-        'control_rnn_size': wandb.config['control_rnn_size'],
-        'control_rnn_depth': wandb.config['control_rnn_depth'],
-        'encoder_size': wandb.config['encoder_size'],
-        'encoder_depth': wandb.config['encoder_depth'],
-        'decoder_size': wandb.config['decoder_size'],
-        'decoder_depth': wandb.config['decoder_depth'],
-        'use_batch_norm': False,
+        "state_dim": train_data.state_dim,
+        "control_dim": train_data.control_dim,
+        "output_dim": train_data.output_dim,
+        "control_rnn_size": wandb.config["control_rnn_size"],
+        "control_rnn_depth": wandb.config["control_rnn_depth"],
+        "encoder_size": wandb.config["encoder_size"],
+        "encoder_depth": wandb.config["encoder_depth"],
+        "decoder_size": wandb.config["decoder_size"],
+        "decoder_depth": wandb.config["decoder_depth"],
+        "use_batch_norm": False,
     }
 
     model_metadata = {
-        'args': model_args,
-        'data_path': data_path.absolute().as_posix(),
-        'data_settings': data["settings"],
-        'data_args': data["args"]
+        "args": model_args,
+        "data_path": data_path.absolute().as_posix(),
+        "data_settings": data["settings"],
+        "data_args": data["args"],
     }
-    model_name = f"flumen-{data_path.stem}-{run.id}"
+
+    model_name = f"flumen-{timestamp}-{data_path.stem}-{run.id}"
 
     # Prepare for saving the model
     model_save_dir = Path(
-        f"./outputs/{first_name}/{full_name}_{run.id}")
+        f"{sys_args.outdir}/{first_name}/{full_name}_{run.id}"
+    )
     model_save_dir.mkdir(parents=True, exist_ok=True)
+    print(f"Writing to directory {model_save_dir}", file=stderr)
 
     # Save local copy of metadata
-    with open(model_save_dir / "metadata.yaml", 'w') as f:
+    with open(model_save_dir / "metadata.yaml", "w") as f:
         yaml.dump(model_metadata, f)
 
     model = CausalFlowModel(**model_args)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
-    optimiser = torch.optim.Adam(model.parameters(), lr=wandb.config['lr'])
+    optimiser = torch.optim.Adam(model.parameters(), lr=wandb.config["lr"])
     sched = torch.optim.lr_scheduler.ReduceLROnPlateau(
         optimiser,
-        patience=wandb.config['sched_patience'],
+        patience=wandb.config["sched_patience"],
         cooldown=0,
-        factor=1. / wandb.config['sched_factor'])
+        factor=1.0 / wandb.config["sched_factor"],
+    )
 
     loss = get_loss(wandb.config["loss"]).to(device)
 
-    early_stop = EarlyStopping(es_patience=wandb.config['es_patience'],
-                               es_delta=wandb.config['es_delta'])
+    early_stop = EarlyStopping(
+        es_patience=wandb.config["es_patience"],
+        es_delta=wandb.config["es_delta"],
+    )
 
-    bs = wandb.config['batch_size']
+    bs = wandb.config["batch_size"]
     train_dl = DataLoader(train_data, batch_size=bs, shuffle=True)
     val_dl = DataLoader(val_data, batch_size=bs, shuffle=True)
     test_dl = DataLoader(test_data, batch_size=bs, shuffle=True)
-
-    header_msg = f"{'Epoch':>5} :: {'Loss (Train)':>16} :: " \
-        f"{'Loss (Val)':>16} :: {'Loss (Test)':>16} :: {'Best (Val)':>16}"
-
-    print(header_msg)
-    print('=' * len(header_msg))
 
     # Evaluate initial loss
     model.eval()
     train_loss = validate(train_dl, loss, model, device)
     val_loss = validate(val_dl, loss, model, device)
     test_loss = validate(test_dl, loss, model, device)
-
     early_stop.step(val_loss)
-    print(
-        f"{0:>5d} :: {train_loss:>16e} :: {val_loss:>16e} :: " \
-        f"{test_loss:>16e} :: {early_stop.best_val_loss:>16e}"
-    )
+
+    print_header()
+    print_losses(0, train_loss, val_loss, test_loss, early_stop.best_val_loss)
 
     last_save_epoch = 0
 
     start = time.time()
 
-    for epoch in range(wandb.config['n_epochs']):
+    for epoch in range(wandb.config["n_epochs"]):
         model.train()
         for example in train_dl:
             train_step(example, loss, model, optimiser, device)
@@ -163,9 +181,8 @@ def main():
         sched.step(val_loss)
         early_stop.step(val_loss)
 
-        print(
-            f"{epoch + 1:>5d} :: {train_loss:>16e} :: {val_loss:>16e} :: " \
-            f"{test_loss:>16e} :: {early_stop.best_val_loss:>16e}"
+        print_losses(
+            epoch + 1, train_loss, val_loss, test_loss, early_stop.best_val_loss
         )
 
         if early_stop.best_model:
@@ -180,14 +197,16 @@ def main():
             run.summary["best_test"] = test_loss
             run.summary["best_epoch"] = epoch + 1
 
-        wandb.log({
-            'time': time.time() - start,
-            'epoch': epoch + 1,
-            'lr': optimiser.param_groups[0]['lr'],
-            'train_loss': train_loss,
-            'val_loss': val_loss,
-            'test_loss': test_loss,
-        })
+        wandb.log(
+            {
+                "time": time.time() - start,
+                "epoch": epoch + 1,
+                "lr": optimiser.param_groups[0]["lr"],
+                "train_loss": train_loss,
+                "val_loss": val_loss,
+                "test_loss": test_loss,
+            }
+        )
 
         if early_stop.early_stop:
             print(f"{epoch + 1:>5d} :: --- Early stop ---")
@@ -201,6 +220,6 @@ def main():
     print(f"Training took {train_time:.2f} seconds.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print_gpu_info()
     main()
