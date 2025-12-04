db284ccd89b6834f2679348ab815b10a9ca19e21
mcpca <mcpcaguiar@gmail.com>
2025-12-04 19:43:09 +0100

    utils: remove unused functions

--- src/flumen/__init__.py
+++ src/flumen/__init__.py
@@ -1,4 +1,4 @@
 from .model import CausalFlowModel
 from .trajectory import TrajectoryDataset, RawTrajectoryDataset
 from .train import validate
-from .utils import get_arg_parser, pack_model_inputs, print_gpu_info
+from .utils import print_gpu_info
