9ed0273e4acb4fb465d4fda0c0fe744c0a5f25c4
mcpca <mcpcaguiar@gmail.com>
2025-12-07 07:24:16 +0100

    utils: add utility functions to convert arrays to model inputs.

--- src/flumen/utils.py
+++ src/flumen/utils.py
@@ -16,7 +16,31 @@ def print_gpu_info():
             print(msg)
 
 
-def pack_model_inputs(x0, t, u, delta):
+def get_batch_inputs(x0, t, u, delta: float):
+    x0, u, skips, tau = pack_model_inputs(x0, t, u, delta)
+
+    tau_seq = torch.ones((u.shape[0], u.shape[1], 1))
+    tau_seq[range(tau_seq.shape[0]), skips] = tau
+    rnn_input = torch.cat((u, tau_seq), dim=-1)
+
+    lengths = skips.add(1)
+    if lengths.ndim == 0:
+        lengths.unsqueeze(0)
+
+    return x0, rnn_input, tau, lengths
+
+
+def get_batch_inputs_packed(x0, t, u, delta: float):
+    x0, rnn_input, tau, lengths = get_batch_inputs(x0, t, u, delta)
+
+    rnn_input = torch.nn.utils.rnn.pack_padded_sequence(
+        rnn_input, lengths, batch_first=True, enforce_sorted=False
+    )
+
+    return x0, rnn_input, tau
+
+
+def pack_model_inputs(x0, t, u, delta: float):
     t = torch.Tensor(t)
     x0 = torch.Tensor(x0)
     u = torch.Tensor(u)
@@ -25,7 +49,7 @@ def pack_model_inputs(x0, t, u, delta):
         x0 = x0.unsqueeze(0)
         u = u.unsqueeze(0)
 
-    skips = torch.floor(t / delta).int()
+    skips = torch.floor(t / delta).long()
     tau = (t - delta * skips) / delta
 
     return x0, u, skips.squeeze(), tau
